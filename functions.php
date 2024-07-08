24826cfad7febfea30e4e26e6d7856661b1b1b5e
jainish@beardog.digital <jainish@beardog.digital>
2024-07-08 15:00:03 +0530

    Disabled gutenberg

--- functions.php
+++ functions.php
@@ -271,6 +271,21 @@ return $result;
 add_filter('wpcf7_validate_tel','custom_phone_validation', 10, 2);
 add_filter('wpcf7_validate_tel*', 'custom_phone_validation', 10, 2);
 
+// Disable Gutenberg on the back end.
+add_filter( 'use_block_editor_for_post', '__return_false' );
+// Disable Gutenberg for widgets.
+add_filter( 'use_widgets_block_editor', '__return_false' );
+add_action( 'wp_enqueue_scripts', function() {
+    // Remove CSS on the front end.
+    wp_dequeue_style( 'wp-block-library' );
+    // Remove Gutenberg theme.
+    wp_dequeue_style( 'wp-block-library-theme' );
+    // Remove inline global CSS on the front end.
+    wp_dequeue_style( 'global-styles' );
+    // Remove classic-themes CSS for backwards compatibility for button blocks.
+    wp_dequeue_style( 'classic-theme-styles' );
+}, 20 );
+
 /**
  * Custom template tags for this theme.
  */
